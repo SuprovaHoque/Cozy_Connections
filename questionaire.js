21bf90314c3919c8e2378cf40e0ae2813cbfa203
Farida152005 <123782353+Farida152005@users.noreply.github.com>
2023-11-12 11:21:38 -0700

    Update questionaire.js

--- questionaire.js
+++ questionaire.js
@@ -5,14 +5,15 @@ function updateTextInput(val) {
   }
 function submitForm(){
     const numItems = document.getElementById('amountToDonateInput').value;
-    const boroughItem = document.querySelector('input[name="area"]:checked').value;    const genderCheckboxes = document.querySelectorAll('input[name="gender"]:checked');
+    const boroughItem = document.querySelector('input[name="area"]:checked').value;    
+    const genderCheckboxes = document.querySelectorAll('input[name="gender"]:checked');
     const selectedGenders = [];
 
     // Iterate through checkboxes and add checked ones to the array
     genderCheckboxes.forEach(checkbox => {
         selectedGenders.push(checkbox.value);
     });
-    document.write(numItems,selectedGenders.length, boroughItem);
+
     if (!numItems || selectedGenders.length == 0 || !boroughItem) {
         alert('Please fill out all fields');
         return;
@@ -24,4 +25,5 @@ function submitForm(){
         selectedGenders:selectedGenders,
     };
     userInputsList.push(userInput);
+    window.location.href = "results.html";
 }
