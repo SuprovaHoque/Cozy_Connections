f8112c9ea3b8786b8476f20313500b342b752b22
Farida152005 <123782353+Farida152005@users.noreply.github.com>
2023-11-11 23:00:35 -0700

    Update questionaire.js

--- questionaire.js
+++ questionaire.js
@@ -1,28 +1,27 @@
+
 const userInputsList = [];
 function updateTextInput(val) {
     document.getElementById('textInput').value=val; 
   }
 function submitForm(){
-    const numItems = document.getElementById('amountToDonate').value;
+    const numItems = document.getElementById('amountToDonateInput').value;
+    const boroughItem = document.querySelector('input[name="area"]:checked').value;    const genderCheckboxes = document.querySelectorAll('input[name="gender"]:checked');
     const selectedGenders = [];
 
     // Iterate through checkboxes and add checked ones to the array
-    for (let i = 0; i < genderCheckboxes.length; i++) {
-        if (genderCheckboxes[i].checked) {
-            selectedGenders.push(genderCheckboxes[i].value);
-        }
-    }
-    const boroughItem = document.getElementById('borough').value;
-
-    if (!numItems || selectedGenders.length === 0 ||!boroughItem) {
+    genderCheckboxes.forEach(checkbox => {
+        selectedGenders.push(checkbox.value);
+    });
+    document.write(numItems,selectedGenders.length, boroughItem);
+    if (!numItems || selectedGenders.length == 0 || !boroughItem) {
         alert('Please fill out all fields');
         return;
     }
 
     const userInput={
         numItems:numItems,
-        genderAndAgeItems:genderAndAgeItems,
         boroughItem:boroughItem,
+        selectedGenders:selectedGenders,
     };
     userInputsList.push(userInput);
 }
